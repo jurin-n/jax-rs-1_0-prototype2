ec4445ff0e8efa3de6c90747a445e06132647615
jurin-n <jurin.nakano@gmail.com>
2015-11-29 16:19:56 +0900

    パラメータ化テストにしてみた。

--- src/test/java/com/jurin_n/domain/model/practice/PracticePlanIdTest.java
+++ src/test/java/com/jurin_n/domain/model/practice/PracticePlanIdTest.java
@@ -3,48 +3,29 @@ package com.jurin_n.domain.model.practice;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
+@RunWith(Theories.class)
 public class PracticePlanIdTest {
+	@DataPoint
+	public static PracticePlanId id1 = new PracticePlanId("aaaa12345");
+	
+	@DataPoint
+	public static PracticePlanId id2 = new PracticePlanId("aaaa12345");
 
-	@Test
-	public void test等値でないが等価のPracticePlanIdのテスト() {
-		PracticePlanId id1 = new PracticePlanId("aaaa12345");
-		PracticePlanId id2 = new PracticePlanId("aaaa12345");
-		
-		if(id1 == id2) fail();
-		if(   id1.equals(id2) 
-		   && id2.equals(id1)
-		   && id1.equals(id1)
-		   && id2.equals(id2)
-		   && id1.hashCode() == id2.hashCode()
-		   && id1.hashCode() == id1.hashCode()
-		   && id2.hashCode() == id2.hashCode()
-				){
-			return;
-		}
-		fail();
+	@Theory
+	public void testPracticePlanIdの等価判定テスト(
+			 PracticePlanId id1
+			,PracticePlanId id2
+			) {
+		System.out.println("id1.getId()=" + id1.getId() + ",id2.getId()=" + id2.getId());
+		if(!id1.equals(id2)) fail();
+		if(!(id1.hashCode() == id2.hashCode())) fail();
 	}
-	
-	@Test
-	public void test等値のPracticePlanIdのテスト() {
-		PracticePlanId id1 = new PracticePlanId("aaaa12345");
-		PracticePlanId id2 = id1;
-		
-		if(!(id1 == id2)) fail(); //等値でないのでエラー
-		
-		if(   id1.equals(id2) 
-		   && id2.equals(id1)
-		   && id1.equals(id1)
-		   && id2.equals(id2)
-		   && id1.hashCode() == id2.hashCode()
-		   && id1.hashCode() == id1.hashCode()
-		   && id2.hashCode() == id2.hashCode()
-				){
-			return;
-		}
-		fail();
-	}
-	
+
 	@Test
 	public void test文字列表現toStringのテスト() {
 		PracticePlanId id1 = new PracticePlanId("aaaa12345");
