d1efc8d9f05b7ec763f61aa7c1e32d892b187637
jurin-n <jurin.nakano@gmail.com>
2015-12-12 14:32:09 +0900

    fixed #38.

--- src/main/java/com/jurin_n/domain/model/practice/menu/PracticeMenu.java
+++ src/main/java/com/jurin_n/domain/model/practice/menu/PracticeMenu.java
@@ -5,12 +5,19 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.jurin_n.jax_rs.representation.PracticeMenuRepresentation;
+
 @Entity
 @Table(name="t_PracticeMenu")
+@NamedQueries({
+	@NamedQuery(name = "PracticeMenu.FIND_ALL", query = "select p from PracticeMenu p")
+})
 public class PracticeMenu {
 	
 	@EmbeddedId
@@ -34,11 +41,25 @@ public class PracticeMenu {
 		this.updateDate = date;
 	}
 
+	public PracticeMenu(PracticeMenuRepresentation aMenu) {
+		Date date = new Date();
+		if(aMenu.getId()!=null){
+			this.practiceMenuId = new PracticeMenuId(aMenu.getId());
+		}
+		this.menu = aMenu.getMenu();
+		this.createDate = date;
+		this.updateDate = date;
+	}
+
 	public PracticeMenuId getPracticeMenuId() {
 		return practiceMenuId;
 	}
 
 	public String getMenu() {
 		return menu;
+	}
+
+	public void setMemberId(PracticeMenuId aPracticeMenuId) {
+		this.practiceMenuId = aPracticeMenuId;
 	}
 }
