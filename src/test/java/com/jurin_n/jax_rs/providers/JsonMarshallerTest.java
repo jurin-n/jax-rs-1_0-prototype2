b66da6c93b7f5a3906ac839d9fd4fc39bf6b7da1
jurin-n <jurin.nakano@gmail.com>
2015-12-16 00:03:58 +0900

    add Test for JsonMarshallerTest.java.
    テストの書き方わからず、途中まで書いてひとまずコミット。

--- src/test/java/com/jurin_n/jax_rs/providers/JsonMarshallerTest.java
+++ src/test/java/com/jurin_n/jax_rs/providers/JsonMarshallerTest.java
@@ -2,10 +2,18 @@ package com.jurin_n.jax_rs.providers;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jurin_n.jax_rs.representation.PracticeMenuRepresentation;
 
 public class JsonMarshallerTest {
@@ -40,5 +48,26 @@ public class JsonMarshallerTest {
 	public void test_isWriteable_falseになるケース(){
 		Class<?> clazz = String.class;
 		assertThat(sut.isWriteable(clazz, null, null, null), is(false));
+	}
+	
+	@Ignore("テストの書き方わからずorz")
+	@Test
+	public void test_writeTo(){
+        // バイト配列ストリームを作る
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        
+        // バイト配列ストリームをオブジェクトストリームでラップ
+        ObjectOutputStream oout;
+		try {
+			oout = new ObjectOutputStream(bout);
+			sut.writeTo(json, null, null, null, null, null, oout);
+			ObjectMapper mapper = new ObjectMapper();
+			PracticeMenuRepresentation jsonMarshal = mapper.readValue(bout.toString("UTF-8"),PracticeMenuRepresentation.class);
+			System.out.println(jsonMarshal.getId());
+			System.out.println(jsonMarshal.getMenu());
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 }
