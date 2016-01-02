0a89e55776eec9ddefa94ea866e93752be0607a3
jurin-n <jurin.nakano@gmail.com>
2016-01-03 00:05:03 +0900

    RoleとPermissionのセットアップを@Beforeを付与したメソッドに移動。

--- src/test/java/com/jurin_n/domain/model/identity/user/UserTest.java
+++ src/test/java/com/jurin_n/domain/model/identity/user/UserTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.jurin_n.domain.model.identity.Status;
@@ -15,13 +16,26 @@ public class UserTest {
 
 	private String A_USER_ID_FOR_TEST = "user001";
 	private String A_USER_NAME_FOR_TEST = "テスト　太郎";
+	private Set<Role> roles;
+	private Role role;
+	private Set<PermissionValue> permissions;
 
+	@Before
+	public void createRole(){
+		roles = new HashSet<>();
+		permissions = new HashSet<>();
+
+		//writePlanのPermissionを保有するADMINロール作成
+		role = new Role();
+		role.setValue(RoleValue.ADMIN);
+		permissions.add(PermissionValue.writePlan);
+		role.setPermissions(permissions);
+
+		roles.add(role);
+	}
+	
 	@Test
 	public void test_inRoleメソッドの引数に渡したRoleがUserに含まれてる場合_inRoleメソッドはtrueを返す() {
-		Set<Role> roles = new HashSet<>();
-		Role role = new Role();
-		role.setValue(RoleValue.ADMIN);
-		roles.add(role);
 		User user = new User(
 				 new UserId(A_USER_ID_FOR_TEST)
 				,A_USER_NAME_FOR_TEST
@@ -39,19 +53,13 @@ public class UserTest {
 
 	@Test
 	public void test_inPermissionメソッドの引数に渡したPermissionがUserに含まれている場合_inPermissionメソッドはtrueを返す() {
-		Role role = new Role();
-		Set<PermissionValue> permissions = new HashSet<>();
-		permissions.add(PermissionValue.readPlan);
-		role.setPermissions(permissions);
-		Set<Role> roles = new HashSet<>();
-		roles.add(role);
 		User user = new User(
 				 new UserId(A_USER_ID_FOR_TEST)
 				,A_USER_NAME_FOR_TEST
 				,roles
 				,Status.ACTIVE);
 
-		assertTrue(user.inPermission(PermissionValue.readPlan));
+		assertTrue(user.inPermission(PermissionValue.writePlan));
 	}
 	
 	@Test
